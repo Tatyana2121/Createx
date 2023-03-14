16990d1ca241446d3a333c6515470bb903f06ced
Tatyana2121 <gia-celena@rambler.ru>
2023-03-14 18:12:01 +0300

    add adaptive 1

--- js/main.js
+++ js/main.js
@@ -36,4 +36,18 @@ $(function () {
     e.preventDefault(),
     $('.testimonials__slider').slick('slickNext')
   })
+
+  setInterval(() => {
+    if ($(window).scrollTop() > 0 && $('.header__top').hasClass('header__top--open') === false) {
+      $('.burger').addClass('burger--follow')
+    } else {
+      $('.burger').removeClass('burger--follow')
+    }
+  }, 0)
+  $('.burger, .overlay').on('click', function(e) {
+    e.preventDefault()
+    $('.header__top').toggleClass('header__top--open')
+    $('.burger').toggleClass('burger--close')
+    $('.overlay').toggleClass('overlay--show')
+  })
 })
